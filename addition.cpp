417d896fe2606b4462976d38ee7f83bb5a026791
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-29 21:41:36 +0000

    addtion

---
+++ addition.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved
+//
+// Created by Donggeun Lim
+// Created on November 2020
+// This program is addition
+
+#include <iostream>
+
+int main() {
+    // this function calculates area and perimeter
+    int fisrnumber;
+    int seconnummber;
+    int value;
+
+    // input
+    std::cout << "Enter first number :" << std::endl;
+    std::cin >> fisrnumber;
+    std::cout << "Enter seconnummber:" << std::endl;
+    std::cin >> seconnummber;
+
+    // process
+    value = fisrnumber+seconnummber;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "value is " << value << std::endl;
+}
